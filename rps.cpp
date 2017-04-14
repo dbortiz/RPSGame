2715a213149af846da788a0fcda77a0ce73b9c30
dbortiz <dbortiz@csu.fullerton.edu>
2017-04-14 16:43:46 -0700

    Initialized constructors for Rock, Paper, and Scissors classes

--- rps.cpp
+++ rps.cpp
@@ -11,6 +11,18 @@ Tool::~Tool(){
 	cout << "Someone lost." << endl;
 }
 
+Rock::Rock(int r){
+	strength = r;
+}
+
+Paper::Paper(int p){
+	strength = p;
+}
+
+Scissors::Scissors(int s){
+	strength = s;
+}
+
 void Tool::setStrength(int s){
 	strength = s;
 }
