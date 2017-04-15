318b413e03105e1cb23b2d130593a37e55f4d551
dbortiz <dbortiz@csu.fullerton.edu>
2017-04-14 18:01:18 -0700

    Test: print out variables

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 int main(){
 	Tool t;
-	Rock r;
-	Paper p;
-	Scissors s;
+	Rock r(3);
+	Paper p(4);
+	Scissors s(5);
+
+	cout << r.strength << endl;
+	cout << r.type << endl;
+	cout << p.strength << endl;
+	cout << p.type << endl;
+	cout << s.strength << endl;
+	cout << s.type << endl;
 
 	return 0;
 }
