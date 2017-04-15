18e4232bda2f7849e6e91e20986a8e47b505c153
dbortiz <dbortiz@csu.fullerton.edu>
2017-04-14 17:41:40 -0700

    Created default constructors for Rock, Paper, and Scissors classes

--- rps.h
+++ rps.h
@@ -13,18 +13,21 @@ class Tool{
 
 class Rock: public Tool{
 	public:
+		Rock();
 		Rock(int);
 		bool fight(Tool);
 };
 
 class Paper: public Tool{
 	public:
+		Paper();
 		Paper(int);
 		bool fight(Tool);
 };
 
 class Scissors: public Tool{
 	public:
+		Scissors();
 		Scissors(int);
 		bool fight(Tool);
 };
